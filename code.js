2156d01fca3ded97975c241d407103183bdde759
madortill <mador.till@gmail.com>
2021-09-13 08:58:04 +0300

    Update code.js

--- code.js
+++ code.js
@@ -61,10 +61,10 @@ var ArrPages = [
   {
     // assessment- page 7
     divName: ["assessment"],
-    functions: ['goTwoBack(-1)'], 
+    functions: ['goTwoBack(-1)', 'pop_changeTriangle()'], 
     moveButtons: true, 
     lessonMap: true, 
-    robinText: 'כיאה ללוחם אמיץ כמוני, אני משתמש במשולש לח"מ- לומד, חומר ומלמד.<br>לחצו על כל אחד מהמשולשים שמרכיבים את החץ כדי לקרוא עליו (לא תוכלו לעבור עמוד לפני שתלמדו על כולם)'
+    robinText: 'כיאה ללוחם אמיץ כמוני, אני משתמש במשולש לח"מ- לומד, חומר ומלמד.<br>לחצו על כל אחד מהמשולשים שמרכיבים את החץ כדי לקרוא עליו (לא תוכלו לעבור עמוד לפני שתלמדו על כולם).'
   },
   {
     // opening game- page ?
@@ -153,4 +153,34 @@ function pop_buttons(button, page) {
     // shows next page
     movePage();     
   })
+}
+
+const triangle_text = {
+  triangle_1: "החומר- כתבו מהי רמת החומר, האם יש ידע מקדים הנדרש להבנתו, קשיים אפשריים של החניכים וכדומה.<br>המידע יעזור לכם להתכונן לשיעור (לדוגמה אם אתם יודעים שיש הרבה חומר, תכינו אותו יותר זמן מראש.",
+  triangle_2: "הלומד- כתבו את מאפייני הלומד (למשל גיל וסוג אוכלוסייה), קשיי למידה, השלב בו הוא נמצא בהכשרה, נקודות חוזק וחולשה וכדומה.<br>המידע יעזור להתאים את רמת השיעור לקהל היעד (לדוגמה תדעו להשתמש בעזרים חזותיים ובמשלב לשוני נמוך יותר בשיעורים שמועברים לעולים חדשים).",
+  triangle_3: "המלמד- כתבו מהי רמת השליטה שלכם בחומר, מידת ההיכרות עם החיילים, נקודות חוזק וחולשה וכדומה.<br>המידע יעזור להתאים את עצמכם להשגת מטרת השיעור (לדוגמה אם קשה לכם ללמד ברעש, תוכלו לנסות לשלב משחקים או למידה עצמית במהלך השיעור)."
+}
+let colored_triangles = 0;
+
+function pop_changeTriangle() {
+  // disable moving page
+  $(".control-button").css("pointer-events", "none");
+  $(".click-triangle").on("click", function() {
+    let triangle_num = $(event.target).attr("id").slice(-1);
+    // the triangle is colored (only once for each)
+    if ($("#triangle-" + triangle_num).css("opacity") !== "1") {
+      $("#triangle-" + triangle_num).css("opacity", "1");
+      colored_triangles++;
+    }
+    // switch text from the array
+    $("#assessment .speech-bubble").html(triangle_text["triangle_" + triangle_num]);
+    // if all the three triangles are colored
+      if(colored_triangles === Object.keys(triangle_text).length) {
+        $(".control-button").css("pointer-events", "auto");
+        ArrPages[nPage].robinText = 'כיאה ללוחם אמיץ כמוני, אני משתמש במשולש לח"מ- לומד, חומר ומלמד.<br>לחצו על כל אחד מהמשולשים שמרכיבים את החץ כדי לקרוא עליו.';
+        // add to the num of the triangles so it won't enter the condition again
+        colored_triangles++;
+        console.log("loteh");
+      }
+  });
 }
