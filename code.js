9b9b0ebd1aea7d9b3be456845b5491993b01d039
madortill <mador.till@gmail.com>
2021-10-03 11:13:21 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,101 +1,102 @@
 var nPage = 0;
+var currTopic = null;
 
 var ArrPages = [
-  // // opening
-  // {
-  //   // opening game- page 0
-  //   divName: ["opening-game", "general-opening-game"], // the last div contains the speech bubble
-  //   functions: ["openingGame()"], // array of functions that are needed to the page. If the functions contain the word "pop", it will happen only once and will be popped out of the array afterwards
-  //   moveButtons: false, // does this page contains back and next buttons - true/false
-  //   lessonMap: false, // does this page contains lesson map - true/false
-  //   robinText: 'היי! אני הוא רובין הו"ד!<br><br>בבקשה תגררו את החץ הכחול אל החלקה הצהובה' // contains Robin's text (unless it is empty)
-  // },
-  // {
-  //   // opening game question- page 1
-  //   divName: ["opening-game-question", "general-opening-game"],
-  //   functions: ['pop_buttons($("#prev"), -1)', 'pop_buttons($("#next"), +1)', 'clearIntervalExplanation()', 'pop_openingGameQuestion()'],
-  //   moveButtons: true,
-  //   lessonMap: false,
-  //   robinText: 'כל הכבוד! עזרתם לי לפתור את החידה!'
-  // },
-  // {
-  //   // opening game explanation- page 2
-  //   divName: ["opening-game-explanation", "general-opening-game"],
-  //   functions: ['openingGameExplanation()', "setIntervalExplanation = setInterval(openingGameExplanation, 2600);"],
-  //   moveButtons: true,
-  //   lessonMap: false,
-  //   robinText: 'למעשה על ידי גרירה של החץ האדום למיקום המיועד הצלחתם להפוך את התרגיל המתמטי הנתון לתרגיל הגיוני'
-  // },
-  // {
-  //   // opening game conclusion- page 3
-  //   divName: ["opening-game-conclusion"],
-  //   functions: ['clearIntervalExplanation()'],
-  //   moveButtons: true,
-  //   lessonMap: false,
-  //   robinText: "אילו הייתם מבינים מה <span>המטרה</span> של הפעולה, הייתם נהנים יותר במהלך פתרון החידה, ולא הייתם מרגישים שאתם מבזבזים את הזמן.<br><br> כך בדיוק נבנה גם שיעור- אם אין לו מטרות הוא מאבד מהיעילות שלו."
-  // },
-  // {
-  //   // opening game question- page 4
-  //   divName: ["opening"],
-  //   functions: ['pop_opening()', 'pop_buttons($("#play"), 2)', 'pop_buttons($("#about-button"), 1)'],
-  //   moveButtons: false,
-  //   lessonMap: false,
-  //   robinText: "" 
-  // },
-  // {
-  //   // about- page 5
-  //   divName: ["about"],
-  //   functions: [],
-  //   moveButtons: true,
-  //   lessonMap: false,
-  //   robinText: '<img id="till" class="logo" src="assets/media/symbols/tilblack.svg"><div><b>אודות</b></div><b>רמ"ד טיל-</b> רס"ן מיגל לוויתן<br><b>רת"ח מו"פ וחדשנות בלמידה-</b> סמ"ר גל גנסין<br><b>עיצוב גרפי-</b> סמל מייה ליבנה<br><b>תכנות-</b> רב"ט טל סרוסי<br><b>מומחית תוכן-</b> סג"מ שירה רוט<br>גרסה</b>- אוקטובר 2021<b></div>'
-  // },
-  // {
-  //   // lesson's goals- page 6
-  //   divName: ["lesson-goals"],
-  //   functions: ['goTwoBack(-2)', 'pop_goTwoBack_opening()'],
-  //   moveButtons: true,
-  //   lessonMap: true,
-  //   robinText: "וכמו בכל שיעור, נתחיל עם הצגת המטרות!"
-  // },
-  // // assessment
-  // {
-  //   // assessment- page 7
-  //   divName: ["assessment"],
-  //   functions: ['goTwoBack(-1)', 'pop_changeTriangle()', 'pop_buttons($("#first-topic"), ArrPages.findIndex(x => x.divName === ["assessment"]))'], 
-  //   moveButtons: true, 
-  //   lessonMap: true, 
-  //   robinText: 'כיאה ללוחם אמיץ כמוני, אני משתמש במשולש לח"מ- לומד, חומר ומלמד.<br>לחצו על כל אחד מהמשולשים שמרכיבים את החץ כדי לקרוא עליו (לא תוכלו לעבור עמוד לפני שתלמדו על כולם).'
-  // },
-  // // goals
-  // {
-  //   // importance of goals- page 8
-  //   divName: ["importance"],
-  //   functions: [""], 
-  //   moveButtons: true, 
-  //   lessonMap: true, 
-  //   robinText: 'עכשיו אפשר לכתוב מטרות!<br>אבל למה צריך אותן?'
-  // },
-  // {
-  //   // types of goals- page 9
-  //   divName: ["types"],
-  //   functions: [""], 
-  //   moveButtons: true, 
-  //   lessonMap: true, 
-  //   robinText: ''
-  // },
-  // {
-  //   // wording rules- page 10
-  //   divName: ["wording-rules"],
-  //   functions: [""], 
-  //   moveButtons: true, 
-  //   lessonMap: true, 
-  //   robinText: ''
-  // },
+  // opening
+  {
+    // opening game- page 0
+    divName: ["opening-game", "general-opening-game"], // the last div contains the speech bubble
+    functions: ["openingGame()"], // array of functions that are needed to the page. If the functions contain the word "pop", it will happen only once and will be popped out of the array afterwards
+    moveButtons: false, // does this page contains back and next buttons - true/false
+    lessonMap: false, // does this page contains lesson map - true/false
+    robinText: 'היי! אני הוא רובין הו"ד!<br><br>בבקשה תגררו את החץ הכחול אל החלקה הצהובה' // contains Robin's text (unless it is empty)
+  },
+  {
+    // opening game question- page 1
+    divName: ["opening-game-question", "general-opening-game"],
+    functions: ['pop_buttons($("#prev"), -1, "add")', 'pop_buttons($("#next"), +1, "add")', 'clearIntervalExplanation()', 'pop_openingGameQuestion()'],
+    moveButtons: true,
+    lessonMap: false,
+    robinText: 'כל הכבוד! עזרתם לי לפתור את החידה!'
+  },
+  {
+    // opening game explanation- page 2
+    divName: ["opening-game-explanation", "general-opening-game"],
+    functions: ['openingGameExplanation()', "setIntervalExplanation = setInterval(openingGameExplanation, 2600);"],
+    moveButtons: true,
+    lessonMap: false,
+    robinText: 'למעשה על ידי גרירה של החץ האדום למיקום המיועד הצלחתם להפוך את התרגיל המתמטי הנתון לתרגיל הגיוני'
+  },
+  {
+    // opening game conclusion- page 3
+    divName: ["opening-game-conclusion"],
+    functions: ['clearIntervalExplanation()'],
+    moveButtons: true,
+    lessonMap: false,
+    robinText: "אילו הייתם מבינים מה <span>המטרה</span> של הפעולה, הייתם נהנים יותר במהלך פתרון החידה, ולא הייתם מרגישים שאתם מבזבזים את הזמן.<br><br> כך בדיוק נבנה גם שיעור- אם אין לו מטרות הוא מאבד מהיעילות שלו."
+  },
+  {
+    // opening game question- page 4
+    divName: ["opening"],
+    functions: ['pop_opening()', 'pop_buttons($("#play"), 2, "add")', 'pop_buttons($("#about-button"), 1, "add")'],
+    moveButtons: false,
+    lessonMap: false,
+    robinText: "" 
+  },
+  {
+    // about- page 5
+    divName: ["about"],
+    functions: [],
+    moveButtons: true,
+    lessonMap: false,
+    robinText: '<img id="till" class="logo" src="assets/media/symbols/tilblack.svg"><div><b>אודות</b></div><b>רמ"ד טיל-</b> רס"ן מיגל לוויתן<br><b>רת"ח מו"פ וחדשנות בלמידה-</b> סמ"ר גל גנסין<br><b>עיצוב גרפי-</b> סמל מייה ליבנה<br><b>תכנות-</b> רב"ט טל סרוסי<br><b>מומחית תוכן-</b> סג"מ שירה רוט<br>גרסה</b>- אוקטובר 2021<b></div>'
+  },
+  {
+    // lesson's goals- page 6
+    divName: ["lesson-goals"],
+    functions: ['goTwoBack(-2)', 'pop_goTwoBack_opening()', 'changeTopic(null)'],
+    moveButtons: true,
+    lessonMap: true,
+    robinText: "וכמו בכל שיעור, נתחיל עם הצגת המטרות!"
+  },
+  // assessment
+  {
+    // assessment- page 7
+    divName: ["assessment"],
+    functions: ['goTwoBack(-1)', 'pop_changeTriangle()', 'pop_buttons($("#topic-1"), ArrPages.findIndex(x => x.divName.includes("assessment")), "equal")', 'changeTopic(1)'], 
+    moveButtons: true, 
+    lessonMap: true, 
+    robinText: 'כיאה ללוחם אמיץ כמוני, אני משתמש במשולש לח"מ- לומד, חומר ומלמד.<br>לחצו על כל אחד מהמשולשים שמרכיבים את החץ כדי לקרוא עליו (לא תוכלו לעבור עמוד לפני שתלמדו על כולם).'
+  },
+  // goals
+  {
+    // importance of goals- page 8
+    divName: ["importance"],
+    functions: ['pop_buttons($("#topic-2"), ArrPages.findIndex(x => x.divName.includes("importance")), "equal")', 'changeTopic(2)'], 
+    moveButtons: true, 
+    lessonMap: true, 
+    robinText: 'עכשיו אפשר לכתוב מטרות!<br>אבל למה צריך אותן?'
+  },
+  {
+    // types of goals- page 9
+    divName: ["types"],
+    functions: [""], 
+    moveButtons: true, 
+    lessonMap: true, 
+    robinText: ''
+  },
+  {
+    // wording rules- page 10
+    divName: ["wording-rules"],
+    functions: [""], 
+    moveButtons: true, 
+    lessonMap: true, 
+    robinText: ''
+  },
   {
     // bow matching exercise- page 11
     divName: ["bow-match-exer"],
-    functions: [''],
+    functions: ['pop_changeBow($("#prev-bow"), -1)', 'pop_changeBow($("#next-bow"), 1)', 'bowDrag()'],
     moveButtons: false, 
     lessonMap: false, 
     robinText: "אני צריך שתעזרו לי להתאים בין החיצים לקשתות! גררו את המטרות בניסוח השגוי (חץ) לכלל הניסוח שהופר (הקשת)"
@@ -176,45 +177,45 @@ function controls(state, object) {
 }
 
 // function that adds events listeners to buttons that affects the page's display- called only one time for each button
-function pop_buttons(button, page) {
+function pop_buttons(button, number, operation) {
   button.on("click", function() {
     // hides last divs
     for (let i = 0; i < ArrPages[nPage].divName.length; i++) {
       $("#" + ArrPages[nPage].divName[i]).css("display", "none");
     }
     // changes page counter
-    nPage = nPage + page;
+    // if the operation is to add, the number is added to page counter
+    if (operation === "add"){
+      nPage = nPage + eval(number);
+    }
+    // if the operation is to equal, page counter is compared to the number 
+    else if (operation === "equal") {
+      nPage = eval(number);
+    }
     // shows next page
     movePage();     
   })
 }
 
-const triangle_text = {
-  triangle_1: "החומר- כתבו מהי רמת החומר, האם יש ידע מקדים הנדרש להבנתו, קשיים אפשריים של החניכים וכדומה.<br>המידע יעזור לכם להתכונן לשיעור (לדוגמה אם אתם יודעים שיש הרבה חומר, תכינו אותו יותר זמן מראש.",
-  triangle_2: "הלומד- כתבו את מאפייני הלומד (למשל גיל וסוג אוכלוסייה), קשיי למידה, השלב בו הוא נמצא בהכשרה, נקודות חוזק וחולשה וכדומה.<br>המידע יעזור להתאים את רמת השיעור לקהל היעד (לדוגמה תדעו להשתמש בעזרים חזותיים ובמשלב לשוני נמוך יותר בשיעורים שמועברים לעולים חדשים).",
-  triangle_3: "המלמד- כתבו מהי רמת השליטה שלכם בחומר, מידת ההיכרות עם החיילים, נקודות חוזק וחולשה וכדומה.<br>המידע יעזור להתאים את עצמכם להשגת מטרת השיעור (לדוגמה אם קשה לכם ללמד ברעש, תוכלו לנסות לשלב משחקים או למידה עצמית במהלך השיעור)."
-}
-let colored_triangles = 0;
-
-function pop_changeTriangle() {
-  // disable moving page
-  $(".control-button").css("pointer-events", "none");
-  $(".click-triangle").on("click", function() {
-    let triangle_num = $(event.target).attr("id").slice(-1);
-    // the triangle is colored (only once for each)
-    if ($("#triangle-" + triangle_num).css("opacity") !== "1") {
-      $("#triangle-" + triangle_num).css("opacity", "1");
-      colored_triangles++;
-    }
-    // switch text from the array
-    $("#assessment .speech-bubble").html(triangle_text["triangle_" + triangle_num]);
-    // if all the three triangles are colored
-      if(colored_triangles === Object.keys(triangle_text).length) {
-        $(".control-button").css("pointer-events", "auto");
-        ArrPages[nPage].robinText = 'כיאה ללוחם אמיץ כמוני, אני משתמש במשולש לח"מ- לומד, חומר ומלמד.<br>לחצו על כל אחד מהמשולשים שמרכיבים את החץ כדי לקרוא עליו.';
-        // add to the num of the triangles so it won't enter the condition again
-        colored_triangles++;
-        console.log("loteh");
+// function that changes the lesson map according to the user's progress
+changeTopic = newTopic => {
+  // changing the previous topic
+  if (currTopic !== null) {
+    $(`#topic-${currTopic} .topic-text`).css("color", "rgb(60, 60, 59)");
+    // if it has not been visited before
+    if (!$(`#topic-${currTopic} img`).attr("src").includes("finish")) {
+      if (newTopic > currTopic) {
+        $(`#topic-${currTopic} img`).attr("src", "assets/media/map_finish.svg");
       }
-  });
+    }
+  }
+  currTopic = newTopic;
+  if (currTopic !== null) {
+    // changing the current topic
+    $(`#topic-${currTopic} .topic-text`).css("color", "rgb(227, 2, 15)");
+    // if it has not been visited before
+    if (!$(`#topic-${currTopic} img`).attr("src").includes("finish")) {
+      $(`#topic-${currTopic} img`).attr("src", "assets/media/map_select.svg");
+    }
+  }
 }
