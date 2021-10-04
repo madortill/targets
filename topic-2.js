5716aced0308c8dc45c44958cc9ece3a2f5890ec
madortill <mador.till@gmail.com>
2021-10-04 12:56:41 +0300

    Update topic-2.js

--- topic-2.js
+++ topic-2.js
@@ -23,6 +23,12 @@ const arrBowsText = [
 pop_changeBow = (button, bowNum) => {
   // function that changes the carrousel of the bows exercise
   button.on("click", () => {
+    changeBow(bowNum);
+  });
+}
+
+changeBow = (bowNum) => {
+    if (arrBowsText.length > 1) {
     // previous bow disappear
     $(`#bow-${arrBowsText[currBow].bowNumber}`).css("display", "none");
     $(`#bow-${arrBowsText[currBow].bowNumber}`).css("opacity", "0");
@@ -35,11 +41,21 @@ pop_changeBow = (button, bowNum) => {
       currBow = currBow + bowNum;
     }
     // current bow changes
+    
     $(`#bow-${arrBowsText[currBow].bowNumber}`).css("display", "block");
     $(`#bow-${arrBowsText[currBow].bowNumber}`).animate({opacity: "1"}, 450);
     $(`#bow-number`).text(`${arrBowsText[currBow].bowNumber}/4`);
     $(`#bow-title`).text(arrBowsText[currBow].bowTitle);
-  })
+    }
+    // if all the bows are matched 
+    else {
+        $(`#arrows-container`).css("display", "none");
+        $("#bow-match-exer .speech-bubble").text("אתם חדים כמו חץ!");
+        // display prev and next buttons
+        $("#controls").css("display", "flex");
+        $("#controls .control-button").css("display", "block");
+        ArrPages[nPage-1].functions.push('pop_removeExer(1)');
+    }
 }
 
 bowDrag = () => {
@@ -48,11 +64,32 @@ bowDrag = () => {
     revertDuration: 200,
     containment: "parent"
   });
-
-  $("#bow-match-exer #bow-drop").droppable({
-    tolerance: "touch",
-    drop: function(e,ui) {
-      $(this).css("background-color", "white");
-    }
-  });
+  for (let i = 1; i <= arrBowsText.length; i++) {
+    $("#bow-match-exer #bow-" + i + " .bow-drop").droppable({
+        tolerance: "intersect",
+        accept: $("#arrow-" + i),
+        drop: function(e,ui) {
+          ui.draggable.animate({left: "-100vw"}, 200, function() {
+            // moving to next bow
+            $.when(changeBow(1)).then(function() {
+                // changeBow function raises currBow
+                if (currBow !== 0 ) {
+                    --currBow;
+                } else {
+                    currBow = arrBowsText.length -1;
+                }
+                // removing from array
+                arrBowsText.splice(currBow, 1);
+                if (currBow === arrBowsText.length) {
+                    currBow = 0;
+                }
+                if (arrBowsText.length === 1) {
+                    $("#bow-controls .control-button").css("visibility", "hidden");
+                }
+            }); 
+          });
+        }
+      });
+  }
+  
 }
