0439401b79b3cad0d668a86b499b2b8a38076a21
madortill <mador.till@gmail.com>
2021-10-06 13:28:58 +0300

    Update topic-2.js

--- topic-2.js
+++ topic-2.js
@@ -1,56 +1,96 @@
-// represents the object number in the array
-var currBow = 0;
-const arrBowsText = [
+var exerType;
+// represents the exerType number in the array
+var curr_bow = 0;
+var arr_bow = [
     {
-        bowNumber: 1,
+        number: 1,
         bowTitle: "מנוסחות כביצוע של הלומד"
     },
     {
-        bowNumber: 2, 
+        number: 2, 
         bowTitle: "ברורות וחד משמעיות"
     },
     {
-        bowNumber: 3, 
+        number: 3, 
         bowTitle: "בעלות דרך מדידה"
     },
     {
-        bowNumber: 4,
+        number: 4,
         bowTitle: "ריאליות"
     }
 ];
 
+var curr_quiver = 0;
+var arr_quiver = [
+    {
+        number: 1,
+    },
+    {
+        number: 2, 
+    },
+    {
+        number: 3, 
+    },
+    {
+        number: 4,
+    }
+];
+
+
+
 // function that adds events listeners to bows exrcise carousel's prev and next buttons
-pop_changeBow = (button, bowNum) => {
-  // function that changes the carrousel of the bows exercise
-  button.on("click", () => {
-    changeBow(bowNum);
-  });
+pop_changeCarousel = (button, exerTypeParam, indexNum) => {
+    // function that changes the carrousel of the bows exercise
+    button.on("click", () => {
+      // exerType === "bow"/"quiver"
+      changeCarousel(indexNum, true);
+    });
+    exerType = exerTypeParam;
+    Drag();
 }
 
-changeBow = (bowNum) => {
-    if (arrBowsText.length > 1) {
-    // previous bow disappear
-    $(`#bow-${arrBowsText[currBow].bowNumber}`).css("display", "none");
-    $(`#bow-${arrBowsText[currBow].bowNumber}`).css("opacity", "0");
-    // changing the bow number
-    if (currBow + bowNum < 0 ) {
-      currBow = arrBowsText.length - 1;
-    } else if (currBow + bowNum === arrBowsText.length) {
-      currBow = 0;
-    } else {
-      currBow = currBow + bowNum;
+// generic function that disappears and reveals items in carousels
+changeCarousel = (indexNum, disappear) => {
+    // represents the item that appear and disappear in the html (bow/arrow)
+    let item;
+    if (exerType === "bow") {
+        item = "bow";
+    } else if (exerType === "quiver") {
+        item = "quiver-arrow";
     }
-    // current bow changes
-    
-    $(`#bow-${arrBowsText[currBow].bowNumber}`).css("display", "block");
-    $(`#bow-${arrBowsText[currBow].bowNumber}`).animate({opacity: "1"}, 450);
-    $(`#bow-number`).text(`${arrBowsText[currBow].bowNumber}/4`);
-    $(`#bow-title`).text(arrBowsText[currBow].bowTitle);
+
+    if (eval("arr_" + exerType).length > 1) {
+        // previous item disappear
+        if (disappear === true) {
+            $(`#${item}-${eval("arr_" + exerType)[eval("curr_" + exerType)].number}`).css("display", "none");
+            $(`#${item}-${eval("arr_" + exerType)[eval("curr_" + exerType)].number}`).css("opacity", "0");
+        }
+        // changing the item number
+        if (window["curr_" + exerType] + indexNum < 0 ) {
+            window["curr_" + exerType] = window["arr_" + exerType].length - 1;
+        } else if (window["curr_" + exerType] + indexNum === window["arr_" + exerType].length) {
+            window["curr_" + exerType] = 0;
+        } else {
+            window["curr_" + exerType]  += indexNum;
+        }
+        // current item changes
+        $(`#${item}-${eval("arr_" + exerType)[eval("curr_" + exerType)].number}`).css("display", "block");
+        $(`#${item}-${eval("arr_" + exerType)[eval("curr_" + exerType)].number}`).animate({opacity: "1"}, 450);
+        $(`#${exerType}-number`).text(`${eval("arr_" + exerType)[eval("curr_" + exerType)].number}/4`);
+        // bow title change
+        if (exerType === "bow") {
+            $(`#bow-title`).text(arr_bow[curr_bow].bowTitle);
+        }
     }
-    // if all the bows are matched 
+    // if all the items are matched 
     else {
-        $(`#arrows-container`).css("display", "none");
-        $("#bow-match-exer .speech-bubble").text("אתם חדים כמו חץ!");
+        if (exerType === "bow") {
+            $(`#arrows-container`).css("display", "none");
+            $("#bow-exer .speech-bubble").text("אתם חדים כמו חץ!");
+        } else if (exerType === "quiver") {
+            $("#quiver-exer .speech-bubble").text("בול פגיעה!");
+            $("#quiver-number").css("visibility", "hidden");
+        }
         // display prev and next buttons
         $("#controls").css("display", "flex");
         $("#controls .control-button").css("display", "block");
@@ -58,38 +98,67 @@ changeBow = (bowNum) => {
     }
 }
 
-bowDrag = () => {
-  $("#bow-match-exer .arrow").draggable({
-    revert: "invalid",
-    revertDuration: 200,
-    containment: "parent"
-  });
-  for (let i = 1; i <= arrBowsText.length; i++) {
-    $("#bow-match-exer #bow-" + i + " .bow-drop").droppable({
-        tolerance: "intersect",
-        accept: $("#arrow-" + i),
-        drop: function(e,ui) {
-          ui.draggable.animate({left: "-100vw"}, 200, function() {
-            // moving to next bow
-            $.when(changeBow(1)).then(function() {
-                // changeBow function raises currBow
-                if (currBow !== 0 ) {
-                    --currBow;
-                } else {
-                    currBow = arrBowsText.length -1;
-                }
-                // removing from array
-                arrBowsText.splice(currBow, 1);
-                if (currBow === arrBowsText.length) {
-                    currBow = 0;
-                }
-                if (arrBowsText.length === 1) {
-                    $("#bow-controls .control-button").css("visibility", "hidden");
-                }
-            }); 
-          });
+Drag = () => {
+    $(`#${exerType}-exer .arrow`).draggable({
+        revert: "invalid",
+        revertDuration: 200,
+        // so drag won't get stuck because of css property bottom = 0
+        drag: function( event, ui ) {
+           if (exerType === "quiver") {
+            $(this).css({
+                top: $(this).position().top,
+                bottom: "auto"
+            });
+           }
         }
+        // containment: "parent"
       });
-  }
-  
+
+    for (let i = 1; i <= window["arr_" + exerType].length; i++) {
+    $(`#${exerType}-drop-${i}`).droppable({
+        tolerance: "intersect",
+        accept: $(`#${exerType}-arrow-${i}`),
+        drop: function(e,ui) {
+            if (exerType === "bow") {
+                ui.draggable.animate({left: "-100vw"}, 200, function() {
+                removeItem();
+                });
+            } else if (exerType === "quiver") {
+                ui.draggable.draggable('disable');
+                removeItem();
+            }
+         }
+        });
+    }
+}
+
+// function that removes the item from the array
+removeItem = () => {
+    let disappear;
+    if (exerType === "bow") {
+        disappear = true;
+    } else if (exerType === "quiver") {
+        disappear = false;
+    }
+    // animation of disappear
+    $.when(changeCarousel(1, disappear)).then(function() {
+        // changeBow function raises curr var
+        if (window["curr_" + exerType] !== 0 ) {
+            --window["curr_" + exerType];
+        } else {
+            window["curr_" + exerType] = window["arr_" + exerType].length -1;
+        }
+        // removing from array
+        window["arr_" + exerType].splice( window["curr_" + exerType], 1);
+        if (window["curr_" + exerType] === window["arr_" + exerType].length) {
+            window["curr_" + exerType] = 0;
+        }
+        if (window["arr_" + exerType].length === 1) {
+            $(`#${exerType}-controls .control-button`).css("visibility", "hidden");
+        }
+    }); 
+}
+
+dropDown = () => {
+    $( "#speed" ).selectmenu();
 }
