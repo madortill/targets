148c9525fa76c530d1cc015c37786b977f1d67da
madortill <mador.till@gmail.com>
2021-10-06 22:19:03 +0300

    Update topic-2.js

--- topic-2.js
+++ topic-2.js
@@ -159,6 +159,41 @@ removeItem = () => {
     }); 
 }
 
-dropDown = () => {
-    $( "#speed" ).selectmenu();
+const arrDropDown = ["מטרת העל", "מטרות ביניים", "חייל", "יגדיר", "חייל", "יתרגל", "חייל", "ידע לבצע"];
+var currDropDown = 0;
+
+// function that activates the drop-downs and shows the check button
+dropDownInit = () => {
+    for (let i = 1; i <= arrDropDown.length; i++) {
+        $(`#drop-down-${i}`).selectmenu();
+    }
+    $("#prev").css("display", "none");
+    $("#next").css("display", "none");
+    $("#check").css("display", "block");
+    $("#check").on("click", checkDropDown);
+}
+
+checkDropDown = () => {
+    for (let i = 1; i <= arrDropDown.length; i++) {
+        if ($(`#drop-down-${i}-button .ui-selectmenu-text`).text() === arrDropDown[i-1]) {
+            $(`#drop-down-${i}-button.ui-button`).css("border-color", "rgb(52, 240, 93)");
+            currDropDown++;
+            $(`#drop-down-${i}`).selectmenu("disable");
+            $(`#drop-down-${i}-button.ui-button`).css("opacity", "1");
+        } else if ($(`#drop-down-${i}-button .ui-selectmenu-text`).text() !== "בחר/י...") {
+            $(`#drop-down-${i}-button.ui-button`).css("border-color", "rgb(255, 113, 132)");
+        } 
+    }
+        feedbackDropDown();
+}
+
+feedbackDropDown = () => {
+    if (currDropDown === arrDropDown.length) {
+        $("#prev").css("display", "block");
+        $("#next").css("display", "block");
+        $("#check").css("display", "none");
+        $("#dropdown-exer .speech-bubble").text("!בול פגיעה");
+    } else {
+        $("#dropdown-exer .speech-bubble").text("לא דייקתם... נסו שוב!");
+    }
 }
